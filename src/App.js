cfd6e9adbca770e755225dd4d2ec47b92b3b8949
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-07-20 16:37:45 +0200

    added the landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import About from './pages/About';
+import Home from './pages/Home.';
+import Services from './pages/Services';
+import Pricing from './pages/Pricing.';
+import Blog from './pages/Blogs.';
+import Pages from './pages/Pages';
+import Contact from './pages/Contacts.';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router className="App">
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/About' element={<About /> } />
+        <Route path='/Services' element={<Services />} />
+        <Route path='/Pricing' element={<Pricing />} />
+        <Route path='/Portifolio' element={<Blog />} />
+        <Route path='/Blog' element={<Pages />} />
+        <Route path='/Pages' element={<pages />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React,  { useState } from 'react'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search';
+
+const NavBar = () => {
+
+  const [navbarItemsData, setNavbarItemsData] = useState([{name: 'Home', active: true},{name: 'About', active: false}, {name: 'Services', active: false}, {name: 'Pricing', active: false}, {name: 'portifolio', active: false}, {name: 'Blog', active: false }, {name: 'Pages', active: false}, {name: 'Contact', active: false}])
+
+  const handleLinkClick = (id) =>{
+    setNavbarItemsData(prevNavBarItemsData=>{
+      return prevNavBarItemsData.map(prevNavBarItemData=>{
+        if(prevNavBarItemData.active === true) {
+          return {...prevNavBarItemData, active: false}
+        }
+        return prevNavBarItemData.name === id ? {...prevNavBarItemData, active: true} : prevNavBarItemData;
+      })
+    })
+    
+  }
+
+  const navbarItems = navbarItemsData.map((navbarItemData, index) =>{
+    return <Link to={`/${navbarItemData.name}`} onClick={()=>handleLinkClick(navbarItemData.name)} className={ `mx-4 py-8 border-b-${navbarItemData.active ? '[green]' : 'white' } border-b-4 hover:border-b-[green] text-lg transition duration-500`} key={index}>{navbarItemData.name}</Link>
+  })
+
+  return (
+    <div className='w-full h-24 px-8 flex items-between justify-center gap-[2rem] '>
+      <a  href='/' className='flex-[1] flex items-center justify-center'>
+        <img className='cursor-pointer' src="/assets/images/logo.png" alt="logo"  />
+      </a>
+      <div className='flex-[4] flex gap-8 justify-center items-center '>
+        {navbarItems}
+      </div>
+      <div className='flex-[1] grid place-items-center'>
+        <div className='w-1/5 h-1/2 cursor-pointer grid place-items-center bg-[green]'>
+          <SearchIcon className='text-white' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
